ef04ce60b248d64015e0e04d6dff0f1a3171d69c
Maldu <57748330+AMaldu@users.noreply.github.com>
2024-05-29 15:47:49 +0000

    hw2 finished

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -1,14 +1,20 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("nyctaxi-hw2")
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
         return pickle.load(f_in)
+
+mlflow.sklearn.autolog()
+
 
 
 @click.command()
@@ -17,8 +23,9 @@ def load_pickle(filename: str):
     default="./output",
     help="Location where the processed NYC taxi trip data was saved"
 )
-def run_train(data_path: str):
 
+def run_train(data_path: str):
+    
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
@@ -27,6 +34,7 @@ def run_train(data_path: str):
     y_pred = rf.predict(X_val)
 
     rmse = mean_squared_error(y_val, y_pred, squared=False)
+    mlflow.log_metric("rmse", rmse)
 
 
 if __name__ == '__main__':
